43cde4fb1f89925131bb03dcc685a0cfc050ed86
linux369 <xerox336699@gmail.com>
2023-02-27 20:40:36 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import BannerSlider from "@/components/BannerSlider/BannerSlider";
+import BrandArea from "@/components/BrandArea/BrandArea";
+import Categories from "@/components/Categories/Categories";
+import CtaArea from "@/components/CtaArea/CtaArea";
+import FunFacts from "@/components/FunFacts/FunFacts";
+import GuideArea from "@/components/GuideArea/GuideArea";
+import Header from "@/components/Header/Header";
+import Layout from "@/components/Layout/Layout";
+import ProjectsArea from "@/components/ProjectsArea/ProjectsArea";
+import React from "react";
+import "../../flow/config";
+
+
+const Home = () => {
+  return (
+    <Layout>
+      <Header />
+      
+      <ProjectsArea />
+      
+    </Layout>
+  );
+};
+
+export default Home;
